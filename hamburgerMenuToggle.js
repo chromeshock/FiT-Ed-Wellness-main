8619caa74293c9389c35ac8deae2895b2490079f
chromeshock <cruzhector124@gmail.com>
2024-11-05 08:26:49 -0500

    updated burger function

--- hamburgerMenuToggle.js
+++ hamburgerMenuToggle.js
@@ -1,18 +1,36 @@
 
-document.querySelector('.hamburger-icon').addEventListener('click', () => {
-  const hamburgerIcon = document.querySelector('.hamburger-icon');
-  const hamburger = document.querySelector('.hamburger');
+document.querySelector('.hamburgerLinks-icon').addEventListener('click', () => {
+  const hamburgerIcon = document.querySelector('.hamburgerLinks-icon');
+  const hamburgerLinks = document.querySelector('.hamburgerLinks');
   const header = document.querySelector('.bigger-header'); // select the header
 
   if (hamburgerIcon.textContent === '☰') {
     hamburgerIcon.textContent = '✖';
-    hamburger.style.visibility = 'visible';
-    hamburger.style.width = '100%';  // Or whatever width you want
+    hamburgerLinks.style.visibility = 'visible';
+    hamburgerLinks.style.width = '100%';  // Or whatever width you want
     header.classList.add('move-down');  // move the header down
   } else {
     hamburgerIcon.textContent = '☰';
-    hamburger.style.visibility = 'hidden';
-    hamburger.style.width = '0';
-    header.classList.remove('move-down'); // move the header back up
+    hamburgerLinks.style.width = '0';
+    setTimeout(() => {
+    hamburgerLinks.style.visibility = 'hidden';
+    }, 500); // Delay setting visibility to allow for width transition
+    header.classList.remove('move-down');
   }
+});
+
+  const hamburgerLinks = document.querySelectorAll('.hamburgerLinks a');
+
+  hamburgerLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+    console.log('Menu link clicked!');
+
+    const hamburgerLinksElement = document.querySelector('.hamburgerLinks');
+    hamburgerLinksElement.style.width = '0';
+    setTimeout(() => {
+      hamburgerLinksElement.style.visibility = 'hidden';
+    }, 500);  // Delay to allow transition before hiding
+    document.querySelector('.hamburgerLinks-icon').textContent = '☰'; // Reset the icon
+    document.querySelector('.bigger-header').classList.remove('move-down'); // Move header back up
+      });
 });
